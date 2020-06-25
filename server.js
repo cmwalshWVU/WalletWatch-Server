e4fb9ab27ae309ea175c463b5a2263b17e1d8dfd
colin walsh <colinwalsh@colins-MacBook-Pro.local>
2020-06-25 12:43:01 -0400

    allow call from ui

--- server.js
+++ server.js
@@ -19,6 +19,13 @@ app.use(
 );
 app.use(bodyParser.json());
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 
